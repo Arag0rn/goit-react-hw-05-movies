900836cb75756888b3fdeba34e1cf95d947617b5
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-09-19 15:34:00 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import { Header } from "pages/Header";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from "react-router-dom";
 
 
